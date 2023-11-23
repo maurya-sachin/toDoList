8bb0f067fcbb0be28f6f7ab523ceca8bf7a1bc7a
sachin-maurya <sachinmaurya1710@gmail.com>
2023-11-23 10:22:09 +0530

    added functions for to dolist

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,12 @@
-function addTask() {
+function handleTaskAction(taskElement, action, paragraph) {
     const taskInput = document.getElementById('taskInput');
-    const task = taskInput.value.trim();
-    if (task) {
-        taskAdd(task);
-    }
-    taskInput.value = '';
-}
-function taskAdd(task) {
-    const contentBox = document.querySelector('.content'),
-        tasks = document.createElement('div');
-    tasks.className = 'list';
-    tasks.innerHTML = ` <p>${task}</p>
-        <div class="buttons">
-        <i class="ri-edit-box-line"></i>
-        <i class="ri-delete-bin-line"></i>
-        <div id="pinned"><i class="ri-pushpin-line"></i></div>
-        </div>`
-    contentBox.appendChild(tasks);
-}
 
-function handleKeyDown(event) {
-    if (event.key === 'Enter') {
-        addTask();
+    if (action === 'edit') {
+        taskInput.value = paragraph.textContent;
+    } else if (action === 'delete') {
+        taskElement.remove();
+    } else if (action === 'pin') {
+        // Implement pin logic here
     }
 }
 
@@ -31,3 +16,41 @@ addButton.addEventListener('click', addTask);
 const taskInput = document.getElementById('taskInput');
 taskInput.addEventListener('keydown', handleKeyDown);
 
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const task = taskInput.value.trim();
+    if (task) {
+        taskAdd(task);
+    }
+    taskInput.value = '';
+}
+
+function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+}
+
+function taskAdd(task) {
+    const contentBox = document.querySelector('.content'),
+        tasks = document.createElement('div');
+    tasks.className = 'list';
+    tasks.innerHTML = ` <p>${task}</p>
+        <div class="buttons">
+        <i class="ri-edit-box-line"></i>
+        <i class="ri-delete-bin-line"></i>
+        <i class="ri-pushpin-line"></i>
+        </div>`;
+    contentBox.appendChild(tasks);
+
+    const paragraph = tasks.querySelector('p');
+
+    const editButton = tasks.querySelector(".ri-edit-box-line");
+    const delButton = tasks.querySelector(".ri-delete-bin-line");
+    const pinButton = tasks.querySelector(".ri-pushpin-line");
+
+    editButton.addEventListener('click', () => handleTaskAction(tasks, 'edit', paragraph));
+    delButton.addEventListener('click', () => handleTaskAction(tasks, 'delete', paragraph));
+    pinButton.addEventListener('click', () => handleTaskAction(tasks, 'pin', paragraph));
+    taskInput.value = ''; // Clear input after adding a task
+}

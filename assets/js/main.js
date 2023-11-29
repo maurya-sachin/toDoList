104ebe02d76ed938893704615efe94a01b7030b9
sachin-maurya <sachinmaurya1710@gmail.com>
2023-11-29 11:43:35 +0530

    added program for edit and check box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,32 @@
-function handleTaskAction(taskElement, action, paragraph) {
+let tasksArray = [];
+
+// Load tasks from local storage on page load
+document.addEventListener('DOMContentLoaded', function () {
+    const storedTasks = localStorage.getItem('tasksArray');
+    if (storedTasks) {
+        tasksArray = JSON.parse(storedTasks);
+        tasksArray.forEach(task => {
+            taskAdd(task.id, task.task, task.completed);
+        });
+    }
+});
+
+function handleTaskAction(taskElement, action, paragraph, checkbox) {
+    const taskId = taskElement.dataset.id;
     const taskInput = document.getElementById('taskInput');
+    const addButton = document.getElementById('addButton');
 
     if (action === 'edit') {
         taskInput.value = paragraph.textContent;
+        addButton.textContent = 'Update';
+        addButton.dataset.taskId = taskId;
+        addButton.removeEventListener('click', addTask);
+        addButton.addEventListener('click', updateTask);
     } else if (action === 'delete') {
+        deleteTask(taskId);
         taskElement.remove();
-    } else if (action === 'pin') {
-        // Implement pin logic here
+    } else if (action === 'complete') {
+        toggleComplete(taskElement, checkbox);
     }
 }
 
@@ -14,43 +34,104 @@ const addButton = document.getElementById('addButton');
 addButton.addEventListener('click', addTask);
 
 const taskInput = document.getElementById('taskInput');
-taskInput.addEventListener('keydown', handleKeyDown);
+taskInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
+
+function taskAdd(taskId, task, completed) {
+    const contentBox = document.querySelector('.content');
+
+    // Check if the task with the same ID already exists
+    if (document.querySelector(`.list[data-id="${taskId}"]`)) {
+        return;
+    }
+
+    const tasks = document.createElement('div');
+    tasks.className = 'list';
+    tasks.dataset.id = taskId;
+    tasks.innerHTML = `<p>${task}</p>
+                              <div class="buttons">
+                              <div class="checkbox-wrapper">
+                                <label for="${taskId}-checkbox" class="rocker rocker-small">
+                                <input type="checkbox" id="${taskId}-checkbox" ${completed ? 'checked' : ''}>
+                                <span class="switch-left">Done</span>
+                                <span class="switch-right">ToDo</span>
+                              </label></div>
+                                <i class="ri-edit-box-line"></i>
+                                <i class="ri-delete-bin-line"></i>
+                              </div>`;
+    contentBox.appendChild(tasks);
+
+    const paragraph = tasks.querySelector('p');
+    const checkbox = tasks.querySelector('input[type="checkbox"]');
+    const editButton = tasks.querySelector(".ri-edit-box-line");
+    const delButton = tasks.querySelector(".ri-delete-bin-line");
+
+    checkbox.addEventListener('change', () => handleTaskAction(tasks, 'complete', paragraph, checkbox));
+    editButton.addEventListener('click', () => handleTaskAction(tasks, 'edit', paragraph, checkbox));
+    delButton.addEventListener('click', () => handleTaskAction(tasks, 'delete', paragraph, checkbox));
+
+    tasksArray.push({ id: taskId, task: task, completed: completed });
+    updateLocalStorage();
+}
+
+function updateLocalStorage() {
+    // Save tasksArray to local storage
+    localStorage.setItem('tasksArray', JSON.stringify(tasksArray));
+}
+
+function updateTask() {
+    const taskId = addButton.dataset.taskId;
+    const taskInput = document.getElementById('taskInput');
+    const editedTask = taskInput.value.trim();
+
+    if (editedTask !== '') {
+        const taskToUpdate = tasksArray.find(task => task.id === taskId);
+        if (taskToUpdate) {
+            taskToUpdate.task = editedTask;
+            const taskElement = document.querySelector(`.list[data-id="${taskId}"] p`);
+            if (taskElement) {
+                taskElement.textContent = editedTask;
+            }
+        }
+
+        taskInput.value = '';
+        addButton.textContent = 'Add';
+        addButton.removeEventListener('click', updateTask);
+        addButton.addEventListener('click', addTask);
+        updateLocalStorage();
+    }
+}
 
 function addTask() {
     const taskInput = document.getElementById('taskInput');
     const task = taskInput.value.trim();
-    if (task) {
-        taskAdd(task);
+
+    if (addButton.textContent === 'Update') {
+        updateTask();
+    } else {
+        if (task) {
+            const taskId = Date.now().toString();
+            taskAdd(taskId, task, false);
+            taskInput.value = '';
+        }
     }
-    taskInput.value = '';
 }
 
-function handleKeyDown(event) {
-    if (event.key === 'Enter') {
-        addTask();
-    }
+function deleteTask(taskId) {
+    tasksArray = tasksArray.filter(task => task.id !== taskId);
+    updateLocalStorage();
 }
 
-function taskAdd(task) {
-    const contentBox = document.querySelector('.content'),
-        tasks = document.createElement('div');
-    tasks.className = 'list';
-    tasks.innerHTML = ` <p>${task}</p>
-        <div class="buttons">
-        <i class="ri-edit-box-line"></i>
-        <i class="ri-delete-bin-line"></i>
-        <i class="ri-pushpin-line"></i>
-        </div>`;
-    contentBox.appendChild(tasks);
-
-    const paragraph = tasks.querySelector('p');
-
-    const editButton = tasks.querySelector(".ri-edit-box-line");
-    const delButton = tasks.querySelector(".ri-delete-bin-line");
-    const pinButton = tasks.querySelector(".ri-pushpin-line");
-
-    editButton.addEventListener('click', () => handleTaskAction(tasks, 'edit', paragraph));
-    delButton.addEventListener('click', () => handleTaskAction(tasks, 'delete', paragraph));
-    pinButton.addEventListener('click', () => handleTaskAction(tasks, 'pin', paragraph));
-    taskInput.value = ''; // Clear input after adding a task
+function toggleComplete(taskElement, checkbox) {
+    const taskId = taskElement.dataset.id;
+    const taskToUpdate = tasksArray.find(task => task.id === taskId);
+    if (taskToUpdate) {
+        taskToUpdate.completed = checkbox.checked;
+        const taskText = taskElement.querySelector('p');
+        taskText.style.textDecoration = taskToUpdate.completed ? 'line-through' : 'none';
+        updateLocalStorage();
+    }
 }
